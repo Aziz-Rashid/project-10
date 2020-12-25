df3182a4ed18bd6d712b5eeafa292f8ccd0cd54e
aziz <axixraxhid@gmail.com>
2020-12-25 17:37:36 +0500

    update

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FormStepper from './Components/FormStepper'
+import './styles.css'
+
+const App = () => {
+    return (
+        <div className="Formik-Forms">
+            <FormStepper />
+        </div>
+    )
+}
+
+export default App
